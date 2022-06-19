2548a1651e5d6bb3b4a3a4d9ced29598adbf4a91
Dimas Ananda <traperwaze@gmail.com>
2022-06-19 10:26:41 +0700

    add loading spinner in home
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

---
+++ src/components/loading/index.js
@@ -0,0 +1,11 @@
+import { h } from  'preact';
+import style from './style.css';
+
+
+const Loading = () => {
+    return (
+        <div class={style.spinner} />
+    )
+}
+
+export default Loading;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,14 +1,35 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import style from './style.css';
+import Loading from '../../components/loading';
 
-const Home = () => (
-	<div class={style['home-container']}>
-		<div class={style.home_card}>
-			<h1 class={style.home_card__title}>Ladoosingh</h1>
-			<h3 class={style.home_card__subtitle}>The Request Inspector</h3>
-			<button class={style.home_card__button}>Generate Hook Link</button>
-		</div>
-	</div>
-);
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+
+  const generateBtnOnClick = async () => {
+    setLoading(true);
+  };
+
+  return (
+    <div class={style['home-container']}>
+      <div class={style.home_card}>
+        <h1 class={style.home_card__title}>Ladoosingh</h1>
+        <h3 class={style.home_card__subtitle}>The Request Inspector</h3>
+        <div class={style.home_card__btn_container}>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              class={style.home_card__button}
+              onClick={generateBtnOnClick}
+            >
+              Generate Hook Link
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
