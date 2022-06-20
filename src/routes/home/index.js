32bef60a65d345b250a9608979664d8f6c58e517
Dimas Ananda <traperwaze@gmail.com>
2022-06-21 04:16:59 +0700

    show error msg on home
    if something unexpected happen after calling API,
    show the error message above the button.
    
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,20 +1,26 @@
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
-import {route} from 'preact-router';
+import { route } from 'preact-router';
 import style from './style.css';
 import Loading from '../../components/loading';
 import { generateHook } from '../../api/api-client';
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const generateBtnOnClick = async () => {
     setLoading(true);
-    generateHook().then((link) => {
-      route(`/inspect/${link}`, true);
-    }).catch((err) => {
-      console.log(err);
-    })
+    setError('');
+    
+    generateHook()
+      .then((link) => {
+        route(`/inspect/${link}`, true);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   };
 
   return (
@@ -22,6 +28,11 @@ const Home = () => {
       <div class={style.home_card}>
         <h1 class={style.home_card__title}>Ladoosingh</h1>
         <h3 class={style.home_card__subtitle}>The Request Inspector</h3>
+        <div
+          class={style.home_card__error + ' ' + (error ? style.show : '')}
+        >
+          {error}
+        </div>
         <div class={style.home_card__btn_container}>
           {loading ? (
             <Loading />
