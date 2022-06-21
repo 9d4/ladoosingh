c0c7f1b4c88022eefbd981a7244ac50be7acfe7c
Dimas Ananda <traperwaze@gmail.com>
2022-06-21 10:19:49 +0700

    Inspect page initial design
    - add header card that show link hook information
    - some css tweaks
    
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- src/routes/inspect/index.js
+++ src/routes/inspect/index.js
@@ -1,9 +1,36 @@
 import { h } from 'preact';
+import style from './style.css';
+import { host } from '../../api/paths';
+import copy from 'copy-to-clipboard';
 
-const Inspect = ({linkID}) => {
+const Inspect = ({ linkID }) => {
+  const linkHook = `${host}/l/${linkID}`;
+
+  const copyLink = () => {
+    copy(linkHook);
+  };
+
   return (
-    <div>{linkID}</div>
-  )
+    <div class={style.container}>
+      <div class={`${style.card} ${style.inspect_header}`}>
+        <div class={style.card_body}>
+          <div class={style.hook_container}>
+            <span>Hook: </span>
+            <a href={linkHook} target="_blank" id="linkHook">
+              {linkHook}
+            </a>
+            <button onClick={copyLink}>Copy</button>
+          </div>
+          <article>
+            Make request to this link to inspect the data. The data will be
+            showed below.
+          </article>
+        </div>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default Inspect;
